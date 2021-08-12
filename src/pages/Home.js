5109b246fbf9db5b63a27feebe776d9c85ce343f
Prashanta <prashanta@Prashantas-MacBook-Pro.local>
2021-08-12 23:43:44 +0300

    Fixed responsive behaviour and organize components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import react from 'react'
 import HomeCarousel from '../components/HomeCarousel';
-import {Card} from '../Card'
-import {VideoComponent} from '../VideoComponent'
+import {Card} from '../components/Card'
+import {VideoComponent} from '../components/VideoComponent'
 
 export default function Home() {
     return(
