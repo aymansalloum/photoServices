63f0508db22a944788bea8cce4547de4fc545043
Prashanta <prashanta@Prashantas-MacBook-Pro.local>
2021-08-13 15:27:46 +0300

    Added Album Grid layout to home page

--- src/components/VideoComponent.js
+++ src/components/VideoComponent.js
@@ -11,4 +11,3 @@ export const VideoComponent = () => {
         </div>
     );
 }
-// export default VideoComponent;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import react from 'react'
 import HomeCarousel from '../components/HomeCarousel';
 import {Card} from '../components/Card'
 import {VideoComponent} from '../components/VideoComponent'
+import { PhotoGrid } from '../components/PhotoGrid';
 
 export default function Home() {
     return(
         <React.Fragment>           
             <HomeCarousel/>
             <Card/>
+            <PhotoGrid/>
             <VideoComponent/>
         </React.Fragment>
     );
